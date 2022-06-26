44d2e20f1173bd1f40e56a562d3e9da29106b8b7
Stefan Nožinić <stefan@lugons.org>
2022-06-26 16:13:47 +0200

    Do multiple iterations of testing the same model

--- bin/testAll.sh
+++ bin/testAll.sh
@@ -4,11 +4,15 @@ SAMPLE_CONFIG_DIR=$1
 THIS_DIR=$(readlink -f $(dirname $0))
 REPORTS_DIR=$2
 
+MAX_REPORTS_PER_CONF=10
 mkdir -p $REPORTS_DIR
 
 for config in $SAMPLE_CONFIG_DIR/*; do 
     REPORT_NAME=$(basename -s .config $config)
-    REPORT=$REPORTS_DIR/$REPORT_NAME.txt 
-    $THIS_DIR/test.sh $config > $REPORT
+    REPORT=$REPORTS_DIR/$REPORT_NAME
+    for ((i=0; i<$MAX_REPORTS_PER_CONF; i++)); do 
+        mkdir -p $REPORT
+        $THIS_DIR/test.sh $config > $REPORT/$i.txt
+    done 
 done 
 
